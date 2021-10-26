2d48cc4a06f599b5d615d3dd223f043eb32ba391
sofia-papa <papa.sofia@outlook.it>
2021-10-26 16:04:05 +0100

    add list item

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,24 @@ Route::get('/comics', function () {
 
 Route::get('/movies', function () {
     return view('movies'); }) -> name('movies');
+
+Route::get('/tv', function () {
+    return view('tv'); }) -> name('tv');
+
+Route::get('/games', function () {
+    return view('games'); }) -> name('games');
+
+Route::get('/collectibles', function () {
+    return view('collectibles'); }) -> name('collectibles');
+
+Route::get('/videos', function () {
+    return view('videos'); }) -> name('videos');
+
+Route::get('/fans', function () {
+    return view('fans'); }) -> name('fans');
+
+Route::get('/news', function () {
+    return view('news'); }) -> name('news');
+
+Route::get('/shop', function () {
+    return view('shop'); }) -> name('shop');
