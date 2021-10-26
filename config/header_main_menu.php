6a0cd96017e87d477780a3691fc0bed862106aa4
sofia-papa <papa.sofia@outlook.it>
2021-10-26 16:03:17 +0100

    menu array

---
+++ config/header_main_menu.php
@@ -0,0 +1,16 @@
+<?php 
+     return [
+         ['text' => 'Home', 'route' => 'home'],
+         ['text' => 'Characters', 'route' => 'characters'],
+         ['text' => 'Comics', 'route' => 'comics'],
+         ['text' => 'Movies', 'route' => 'movies'],
+         ['text' => 'Tv', 'route' => 'tv'],
+         ['text' => 'Games', 'route' => 'games'],
+         ['text' => 'Collectibles', 'route' => 'collectibles'],
+         ['text' => 'Videos', 'route' => 'videos'],
+         ['text' => 'Fans', 'route' => 'fans'],
+         ['text' => 'News', 'route' => 'news'],
+         ['text' => 'Shop', 'route' => 'shop'],
+     ]
+
+; ?>
