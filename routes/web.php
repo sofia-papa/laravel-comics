46bfcd8e9edb1a37a057547772437c3199edb288
sofia-papa <papa.sofia@outlook.it>
2021-10-26 15:27:53 +0100

    import images

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('home'); }) -> name ('home');
 
 Route::get('/characters', function () {
     return view('characters');}) ->name('characters') ;
