674df8b8f7c2f9c60beaee9f2074579c8aa6edfa
sofia-papa <papa.sofia@outlook.it>
2021-11-02 17:52:44 +0000

    cancel jumbo

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,13 +5,13 @@
 @section('contenuto')
        <div id="products">
            <div class="main-container"> 
-             <img class="jumbotron" src="{{ asset('images/jumbotron.jpg')}}" alt="">
                <div class="films">
                   @foreach ($films as $film) 
                   
 
                     <div class="card serie-box">
-                      <a href='{{url("/serie/$loop->index")}}'>
+                      {{-- <a href='{{url("/serie/$loop->index")}}'> --}}
+                        <a href="{{ route('serie', ['id' => $loop->index])}}">
                         <img src="{{$film['thumb']}}" alt="">
                         <h3>{{$film['series']}}</h3>
                       </a>
